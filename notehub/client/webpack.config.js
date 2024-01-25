7b6fd071c02d35ddd6b56d769e8ed6fd50efbd45
shishirsabbir <shishir.sabbir@gmail.com>
2024-01-25 15:15:17 +0800

    sass refactorized

--- notehub/client/webpack.config.js
+++ notehub/client/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist/assets'),
+        path: path.resolve(__dirname, 'dist/assets/scripts'),
         filename: 'bundle.js'
     },
     devServer: {
